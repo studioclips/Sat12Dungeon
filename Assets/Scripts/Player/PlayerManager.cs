cb195a98d5be3ad3bf27d6b841a61a4e0a4ff752
Kiyoharu Goto <mastermonk@studioclips.net>
2022-11-12 10:24:05 +0900

    unitask追加。アニメーション呼び出し追加。

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,27 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerView))]
 public class PlayerManager : MonoBehaviour
 {
     //  アニメーションを呼び出すときに使用する型
     public enum PlayerAnimState
     {
-        Idle = 0,
+        Idle  = 0,
         Front = 1,
-        Left = 2,
-        Right = 3,
-        Back = 4
+        Right = 2,
+        Left  = 3,
+        Back  = 4
     }
+
+    //  プレイヤーの描画コンポーネント
+    private PlayerView _playerView = null;
+    //  １回の移動距離
+    private static readonly float _walkDistance = 32;
+    //  移動中かどうかの判断
+    private bool _isWalking = false;
+    public  bool IsWalking => _isWalking;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerView = GetComponent<PlayerView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _playerView.SetAnimation(PlayerAnimState.Front);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _playerView.SetAnimation(PlayerAnimState.Left);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _playerView.SetAnimation(PlayerAnimState.Right);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _playerView.SetAnimation(PlayerAnimState.Back);
+        }
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    public void WalingStart()
+    {
+        //  すでに移動中ならば何もしない
+        if(_isWalking) return;
+        _isWalking = true;
+        //  移動処理を呼び出す
+        WalingTask().Forget();
+    }
+
+    private async UniTask WalingTask()
+    {
+        await UniTask.Yield();
     }
 }
--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -10,14 +10,26 @@ public class PlayerView : MonoBehaviour
 
     private static readonly int PlayerAnimState = Animator.StringToHash("PlayerAnimState");
 
+    //  現在のアニメーションステータス
+    private PlayerManager.PlayerAnimState _playerAnimState = PlayerManager.PlayerAnimState.Idle;
+
     // Start is called before the first frame update
     void Awake()
     {
         _animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// 指定のアニメーションを呼び出す
+    /// </summary>
+    /// <param name="playerAnimState">呼び出すアニメーションのタイプ</param>
     public void SetAnimation(PlayerManager.PlayerAnimState playerAnimState)
     {
+        //  同じアニメーションの再生中ならば再度リクエストしない
+        if(_playerAnimState == playerAnimState) return;
+        //  アニメーションステータスの変更
+        _playerAnimState = playerAnimState;
+        //  アニメーションリクエスト
         _animator.SetInteger(PlayerAnimState, (int)playerAnimState);
     }
 }
